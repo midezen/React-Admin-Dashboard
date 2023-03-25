e584ca203049b3c1436c8223598e71b0cf1a7c85
Aynoble <victordiya90@gmail.com>
2023-03-25 13:01:35 +0100

    completed chart in home page

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 8000,
+  },
+
+  {
+    name: "Feb",
+    "Active User": 5000,
+  },
+  {
+    name: "Mar",
+    "Active User": 1000,
+  },
+  {
+    name: "Apr",
+    "Active User": 7000,
+  },
+  {
+    name: "May",
+    "Active User": 5000,
+  },
+  {
+    name: "Jun",
+    "Active User": 3000,
+  },
+  {
+    name: "Jul",
+    "Active User": 9000,
+  },
+  {
+    name: "Aug",
+    "Active User": 6000,
+  },
+  {
+    name: "Sep",
+    "Active User": 3000,
+  },
+  {
+    name: "Oct",
+    "Active User": 2000,
+  },
+  {
+    name: "Nov",
+    "Active User": 1000,
+  },
+  {
+    name: "Dec",
+    "Active User": 4000,
+  },
+];
